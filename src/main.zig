73fdc5697dae686fd044c0661b25a0d695487da8
Tom <78365287+TommusZerstoerus@users.noreply.github.com>
2025-02-14 10:26:30 +0100

    feat: stage #ff0

--- src/main.zig
+++ src/main.zig
@@ -2,12 +2,21 @@ const std = @import("std");
 
 pub fn main() !void {
     const stdout = std.io.getStdOut().writer();
-    try stdout.print("$ ", .{});
-
     const stdin = std.io.getStdIn().reader();
     var buffer: [1024]u8 = undefined;
-    const user_input = try stdin.readUntilDelimiter(&buffer, '\n');
 
-    // TODO: Handle user input
-    try stdout.print("{s}: command not found\n", .{user_input});
+    while (true) {
+        try stdout.print("$ ", .{});
+
+        const user_input = stdin.readUntilDelimiterOrEof(&buffer, '\n') catch |err| {
+            std.debug.print("Error reading input: {}\n", .{err});
+            break;
+        };
+
+        if (user_input == null) {
+            break;
+        }
+
+        try stdout.print("{s}: command not found\n", .{user_input.?});
+    }
 }
